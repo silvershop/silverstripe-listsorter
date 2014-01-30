a309dfb38308eaa5a3bcd6217e62647f82012266
Jeremy Shipman <jeremy@burnbright.net>
2014-01-30 15:03:10 +1300

    Tidied up list sorter class. FIX: check sort fields key when confirming request sort value is valid. Improved sorter template to display links better.

--- code/ListSorter.php
+++ code/ListSorter.php
@@ -7,42 +7,46 @@ class ListSorter extends ViewableData{
 
 	private $request;
 	private $sortfields;
+	private $defaultdir = "asc";
 
-	function __construct(SS_HTTPRequest $request, $sortfields){
+	public function __construct(SS_HTTPRequest $request, $sortfields) {
 		$this->request = $request;
 		$this->sortfields = $sortfields;
 	}
 
 	/**
-	 * Current sort
+	 * Current sort value
+	 * @return string|null
 	 */
-	function getCurrent(){
+	public function getCurrent() {
 		$sort = $this->request->getVar('sort');
-		if(in_array($sort, $this->sortfields)){
+		//help stop sql injection
+		if(in_array($sort, $this->sortfields) || 
+			isset($this->sortfields[$sort])){
 			return $sort;
 		}
-		return null;
 	}
 
 	/*
 	 * Current direction
 	 */
-	function getDirection(){
-		$dir = $this->request->getVar('dir');
-		return $dir;
+	public function getDirection() {
+		return Convert::raw2sql($this->request->getVar('dir'));
 	}
 
 	/**
 	 * Get the available sorting options
 	 */
-	function getSorts(){
+	public function getSorts() {
 		$sorts = new ArrayList();
 		foreach($this->sortfields as $sort => $title){
 			if(is_numeric($sort)){
 				$sort = $title;
 			}
 			$iscurrent = ($this->getCurrent() == $sort);
-			$dir = $iscurrent ? $this->oppositeDir($this->getDirection()) : "asc";
+			$dir = $iscurrent ? 
+				($this->getDirection() === "desc" ? "asc" : "desc") :
+				$this->defaultdir;
 			$sorts->push(new ArrayData(array(
 				'Title' => $title,
 				'IsCurrent' => $iscurrent,
@@ -50,39 +54,31 @@ class ListSorter extends ViewableData{
 				'Direction' => $dir
 			)));
 		}
+
 		return $sorts;
 	}
 
 	/**
 	 * Sort the given datalist with the current sort
 	 */
-	function sortList($list){
+	public function sortList($list) {
 		if($current = $this->getCurrent()){
 			$list = $list->sort($current, $this->getDirection());
 		}
+
 		return $list;
 	}
 
 	/**
 	 * Helper for creating sort links
 	 */
-	private function generateLink($field, $direction = null){
+	private function generateLink($field, $direction = null) {
 		$url = Http::setGetVar('sort',$field,null,'&');
 		//TODO: strip "start" pagination parameter, as most users won't want to remain on page 23 when sorting
 		if($direction == 'asc' || $direction == 'desc'){
 			$url = Http::setGetVar('dir',$direction,$url,'&');
 		}
 		return $url;
-	}
-
-	private function oppositeDir($dir){
-		switch($dir){
-			case "asc":
-				return "desc";
-			case "desc":
-				return "asc";
-		}
-		return "asc";
 	}
 
 }
--- templates/Includes/Sorter.ss
+++ templates/Includes/Sorter.ss
@@ -3,9 +3,7 @@
 		<span calss="sorter_label">Sort By:</span>
 		<% loop Sorter.Sorts %>
 			<span class="sorter_option sorter_direction_$Direction">
-				<a href="$Link" title="sort by $Title">
-					$Title
-				</a>
+				<a href="$Link" title="sort by $Title">$Title</a>
 			</span>
 			<% if not Last %>|<% end_if %>
 		<% end_loop %>
